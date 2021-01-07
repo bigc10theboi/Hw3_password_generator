111d5d46b595e2c5649a1bf09a5d339bbd1e5daf
Carter Venne <you@example.com>
2021-01-07 14:57:26 -0600

    started pword generator, initial length prompt

--- 03-Javascript/Develop/script.js
+++ 03-Javascript/Develop/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "1234567890";
-var symbols = " !#$%&';:*+,=-./<>?@()[]^_`{}~| ";
+var specialChar = " !#$%&';:*+,=-./<>?@()[]^_`{}~| ";
 
 // Write password to the #password input
 function writePassword() {
@@ -13,6 +13,12 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+//Password generatoring
+var passCharacters = "";
+var userPassword = "";
+
+var passlength = prompt("How many characters would you like in your password? *length must be between 8 and 128 characters");
 
 
 
