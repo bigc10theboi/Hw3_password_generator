5519f90602c062edd7710f7289106bf67b82db6c
Carter Venne <you@example.com>
2021-01-07 16:18:10 -0600

    proper arrays for variable sets

--- 03-Javascript/Develop/script.js
+++ 03-Javascript/Develop/script.js
@@ -12,11 +12,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Variables categorized into 4 groupings
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "1234567890";
-var specialChar = " !#$%&';:*+,=-./<>?@()[]^_`{}~| ";
+//Variables categorized into 4 arrays
+var lowerCase = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+var upperCase = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = [ '@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
 //Password parameters
 var passCharacters = "";
