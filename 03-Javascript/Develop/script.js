befc6d1fa83e57f7080d3a916bee044eb4d9c62f
Carter Venne <you@example.com>
2021-01-07 18:22:18 -0600

    attempted password generate function

--- 03-Javascript/Develop/script.js
+++ 03-Javascript/Develop/script.js
@@ -8,6 +8,7 @@ var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = [ '@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
 //Password parameters
+
 //Prompt for user input on password length
   var passLength = parseInt(
   prompt("How many characters would you like in your password? *length must be between 8 and 128 characters")
@@ -54,6 +55,57 @@ function getRandom(arr) {
   return randElement;
 }
 
+// Function to generate password with user input
+function generatePassword() {
+var passwordOptions = getPasswordOptions();
+// Variable to store password
+var result = [];
+  
+// Array to store types of characters to include in password
+var possibleChar = [];
+  
+// Array to contain one of each type of chosen character, forcing utilization of each chosen type
+var guaranteedChar = [];
+  
+// Conditional statement that adds array of special characters and adds guaranteed character based on user input
+if (passwordOptions.hasSpecialChar) {
+  possibleChar = possibleChar.concat(specialChar);
+  guaranteedChar.push(getRandom(specialChar));
+}
+  
+// Conditional statement that adds array of numeric characters and adds guaranteed character based on user input
+if (passwordOptions.hasNumeric) {
+  possibleChar = possibleChar.concat(numeric);
+  guaranteedChar.push(getRandom(numeric));
+}
+  
+// Conditional statement that adds array of lowercase characters and adds guaranteed character based on user input
+if (passwordOptions.hasLowerCase) {
+  possibleChar = possibleChar.concat(lowerCase);
+  guaranteedChar.push(getRandom(lowerCase));
+}
+  
+// Conditional statement that adds array of uppercase characters and adds guaranteed character based on user input
+if (passwordOptions.hasUpperCase) {
+  possibleChar = possibleChar.concat(upperCase);
+  guaranteedChar.push(getRandom(upperCase));
+}
+  
+// For loop to iterate password length from the options object, selecting random characters from the array of possibilities and concatenating those into the result variable
+for (var i = 0; i < passwordOptions.passLength; i++) {
+  var possibleChar = getRandom(possibleChar);
+  
+  result.push(possibleChar);
+}
+  
+// Mix in at least one of each guaranteed character in  result
+for (var i = 0; i < guaranteedChar.length; i++) {
+  result[i] = guaranteedChar[i];
+}
+  
+// Transform the result into a string and display
+return result.join('');
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
