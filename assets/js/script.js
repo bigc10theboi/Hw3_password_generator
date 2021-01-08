8de60b8bc41ad9093f76461c3aa5675c7573dc30
Carter Venne <you@example.com>
2021-01-07 21:41:56 -0600

    attempted working/not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,44 +8,46 @@ var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = [ '@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
 //Password parameters
-
+function getPasswordOptions() {
 //Prompt for user input on password length
   var passLength = parseInt(
-  prompt("How many characters would you like in your password? *length must be between 8 and 128 characters")
-);
+    prompt("How many characters would you like in your password? *length must be between 8 and 128 characters")
+  );
 
-if (passLength >= 8 && passLength <= 128) {
- password.length = passLength
+  if (passLength >= 8 && passLength <= 128) {
+    password.length = passLength
 
 //Confirms for user input on variable type
   var hasLowerCase = confirm("Do you want lower case characters in this password?");
   var hasUpperCase = confirm("Do you want upper case characters in this password?");
   var hasNumeric = confirm("Do you want numeric characters in this password?");
   var hasSpecialChar = confirm("Do you want special characters in this password?");
-}
+  }
 
-else {
+  else {
   alert("Specifications not met for password creation. Length must be between 8-128 characters!")
-}
+  }
 
 // Conditional statement checking for at least one type of character
-if (
-  hasSpecialChar === false &
-  hasNumeric === false &
-  hasLowerCase === false &
-  hasUpperCase === false
-) {
-  alert('Must select at least one character type');
-}
+  if (
+    hasSpecialChar === false &
+    hasNumeric === false &
+    hasLowerCase === false &
+    hasUpperCase === false
+  ) {
+    alert('Must select at least one character type');
+  }
 
 // Object to store user input
-var passwordOptions = {
+  var passwordOptions = {
     passLength: passLength,
     hasSpecialChar: hasSpecialChar,
     hasNumeric: hasNumeric,
     hasLowerCase: hasLowerCase,
     hasUpperCase: hasUpperCase
-};
+  };
+return passwordOptions;
+}
 
 // Function for getting a random element from an array
 function getRandom(arr) {
